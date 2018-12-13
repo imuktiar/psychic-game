37b81c920005d270a353755e9af554bcbbc7110a
Muktiar Idrees <imuktiar@hotmail.com>
2018-12-13 17:29:51 -0500

    adding

--- javascript/game.js
+++ javascript/game.js
@@ -2,30 +2,34 @@ var psychicLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o
  var youWin = 0;
  var youLose = 0;
  var guessesLeft = 13;
- var guessedLetters = []; //Array to capture user letter input
+ var guessedLetters = []; //Array for letter input
+ var numbers = 1;
 
  document.onkeyup = function(event) {
-   var yourGuess = event.key; //variable to capute user's guess
-   guessedLetters.push(yourGuess); //This is suppose to capture letters     entered by the user
+   var yourGuess = event.key; // user's guess
+   guessedLetters.push(yourGuess); 
    var psychicAns = psychicLetters[Math.floor(Math.random() *    psychicLetters.length)];
 
-   //If your guess matches the psychic's guess,
+   //If your guess = psychic's guess,
    //a point is added to the "Wins" tally and the guesses tally restarts.
    if (yourGuess === psychicAns) {
      youWin++;
      guessesLeft = 12;
    }
-   //If your does not not match the psychic's guess, your number of  guesses remaining are deducted.
+   //If your does not not match your number of  guesses remaining are deducted.
    else {
      guessesLeft--;
    }
 
-   //If the guesses remaining equals 0, you lose, an increment is added to  the "Losses" tally and the points restart.
+   //If the guesses remaining equals 0, you lose, an increment is added to  the losses tally and the points restart.
    if (guessesLeft === 0){
      youLose++;
      guessesLeft = 12;
-   }
 
-   var html = "<p>Guess what letter I'm thinking of:</p>" + "<p>Wins: " + youWin + "</p>" + "<p>Loses: "+ youLose + "<p>Guesses Remaining: " + guessesLeft + "<p>Guessed Letter: " +  guessedLetters.join(', '); //Show guessed letters here
+}
+   
+   
+
+   var html = "<p>Guess what letter I'm thinking of:</p>" + "<p>Wins: " + youWin + "</p>" + "<p>Loses: "+ youLose + "<p>Guesses Remaining: " + guessesLeft + "<p>Guessed Letter: " +  guessedLetters.join(', '); 
    document.querySelector("#psychicAct").innerHTML = html;
  }
